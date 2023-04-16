9483a83e269c7a1f19bf5e95823485504fb0a5cc
Sbtah <shisastore@protonmail.com>
2023-04-16 20:51:48 +0200

    Added celery_worker management command which restarts workers after code change.

---
+++ polls/management/commands/celery_worker.py
@@ -0,0 +1,35 @@
+import shlex
+import sys
+import subprocess
+import os
+import signal
+import getpass
+
+
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+
+def restart_celery():
+    celery_worker_cmd = "celery -A core worker"
+    # cmd = f"pkill -f {celery_worker_cmd}"
+    pid_cmd = f"ps -f -u {getpass.getuser()} | grep '{celery_worker_cmd}' | grep -v grep | awk '{{print $2}}"
+    if sys.platform == "win32":
+        cmd = "taskkill /f /t /im celery.exe"
+    else:
+        try:
+            celery_pid = int(subprocess.check_output(shlex.split(pid_cmd)))
+            os.kill(celery_pid, signal.SIGTERM)
+        except (subprocess.CalledProcessError, ValueError):
+            print("Process is not running or PID is invalid")
+            pass
+
+    subprocess.call(shlex.split(f"{celery_worker_cmd} --loglevel=info"))
+
+
+class Command(BaseCommand):
+    """Base command for restarting Celery workers."""
+
+    def handle(self, *args, **kwargs):
+        print("Starting Celery worker with autoreload...")
+        autoreload.run_with_reloader(restart_celery)
