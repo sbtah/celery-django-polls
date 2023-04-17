cc6c752c961ee91f250a47029a9669dd238183f1
Sbtah <shisastore@protonmail.com>
2023-04-17 22:09:40 +0200

    Added forms.py with YourForm class.

---
+++ polls/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class YourForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        # ass class to make the fields work with bootrap
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "form-control"
+
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=100)
