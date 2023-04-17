2b248f7367cdbe83aaf44bb06f6bacd784854110
Sbtah <shisastore@protonmail.com>
2023-04-17 22:10:46 +0200

    Added needed urls.

--- polls/urls.py
+++ polls/urls.py
@@ -1,1 +1,12 @@
-from django import path
+from django.urls import path
+from polls.views import subscribe, task_status, webhook_test
+
+
+app_name = "polls"
+
+
+urlpatterns = [
+    path("form/", subscribe, name="form"),
+    path("task_status/", task_status, name="task_status"),
+    path("webhook_test/", webhook_test, name="webhook_test"),
+]
